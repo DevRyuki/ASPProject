b2230cab28a44a528a01d58f0ad8b9852bc543d1
Cristian Cerroni <cristian.cerroni@gmail.com>
2019-04-06 12:55:39 -0400

    Updated from MVC 4 to MVC 5 (Cough Cough *update to vs2017*Cough Cough)

--- ASPProjectRedux/Global.asax.cs
+++ ASPProjectRedux/Global.asax.cs
@@ -18,7 +18,7 @@ namespace ASPProjectRedux
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
