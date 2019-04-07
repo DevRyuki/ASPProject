b15d554d52c4c0b841d2481f61907d05c4e0029e
Sid Dekoning <contact@zenny3d.com>
2019-04-06 23:34:28 -0400

    dfadsfdassASDSsdsadadsfdsfa

--- ASPProjectRedux/Controllers/HomeController.cs
+++ ASPProjectRedux/Controllers/HomeController.cs
@@ -111,7 +111,7 @@ namespace ASPProject.Contollers
         {
             if (Request.Form["Signup"] != null)
             {
-                Regex rxp = new Regex(@"^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$")
+                Regex rxp = new Regex(@"^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$");
                 Regex rxe = new Regex(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
                 if (!rxe.IsMatch(Request.Form["Email"]))
                 {
